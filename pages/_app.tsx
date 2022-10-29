b427534ecbcb39aa1199c583526beb3c059fafb2
sehdez <sergio.hdez.cuevas@gmail.com>
2022-10-29 14:11:17 -0500

    Inicio del proyecto

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { NextUIProvider } from '@nextui-org/react';
+
+import '../styles/globals.css'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <NextUIProvider>
+        <Component {...pageProps} />
+    </NextUIProvider>
+  )
 }
+
+export default MyApp
